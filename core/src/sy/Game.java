82f27e4d85f0f683ae95ca710bef1f9dafce0c7c
BlackPhoenix134 <fabian.hermann@hotmail.com>
2021-04-22 15:57:18 +0200

    added physics steps, redering, object management, loop

--- core/src/sy/Game.java
+++ core/src/sy/Game.java
@@ -3,31 +3,72 @@ package sy;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+import sy.gameObjects.DebugObject;
+import sy.gameObjects.GameObject;
+import sy.gameObjects.GameObjectManager;
+import sy.rendering.RenderPipeline;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private final float TICKS = 1f / 60f;
+	private float tickAccumulation = 0;
+
+	private GameObjectManager gameObjectManager = new GameObjectManager();
+	private RenderPipeline renderPipeline;
+	private OrthographicCamera camera;
+	private ExtendViewport viewport;
+	private World world = new World(new Vector2(0, 0), true);
+
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(800, 600, camera);
+		renderPipeline = new RenderPipeline(new SpriteBatch(), camera, viewport);
+		renderPipeline.updateBatchMatrix();
+		DebugObject obj = gameObjectManager.create(DebugObject.class);
+		obj.setTexture(new Texture("badlogic.jpg"));
+	}
+
+	private void stepWorld(float delta) {
+		gameObjectManager.update(delta);
+	}
+
+	private void stepDraw(float delta) {
+		//other draw code
+		renderPipeline.begin();
+		gameObjectManager.draw(delta, renderPipeline);
+		renderPipeline.end();
 	}
 
 	@Override
 	public void render () {
+		float delta = Gdx.graphics.getDeltaTime();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		tickAccumulation += Math.min(delta, 0.25f);
+		if (tickAccumulation >= TICKS) {
+			tickAccumulation -= TICKS; //takes multi phys misses into account (low fps)
+			stepWorld(delta);
+		}
+		stepDraw(delta);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height, true);
+		renderPipeline.updateBatchMatrix();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		renderPipeline.dispose();
+		world.dispose();
 	}
 }
