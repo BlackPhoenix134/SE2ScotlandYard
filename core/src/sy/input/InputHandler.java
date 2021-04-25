f0c30fb3cba4dd9a0b731d50a362471e08e25e67
Johannes Ladurner <80148992+JohannesLadurner@users.noreply.github.com>
2021-04-25 19:25:34 +0200

    Refactoring, merged CostumInputListener + InputHandler -> InputHandler

--- core/src/sy/input/InputHandler.java
+++ core/src/sy/input/InputHandler.java
@@ -1,15 +1,81 @@
 package sy.input;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 
-public class InputHandler {
+public class InputHandler extends InputAdapter implements GestureDetector.GestureListener {
 
     public InputHandler(){
         InputMultiplexer multiplexer = new InputMultiplexer();
-        multiplexer.addProcessor(new CustomInputListener());
-        multiplexer.addProcessor(new GestureDetector(new CustomInputListener()));
+        multiplexer.addProcessor(this);
+        multiplexer.addProcessor(new GestureDetector(this));
         Gdx.input.setInputProcessor(multiplexer);
+    }
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Gdx.app.log("InputHandler", "You touched Pos X: "+screenX+", Y: "+screenY);
+        return super.touchDown(screenX,screenY,pointer,button);
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        Gdx.app.log("InputHandler", "You left the screen at Pos X: "+screenX+", Y: "+screenY);
+        return super.touchUp(screenX, screenY, pointer, button);
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        Gdx.app.log("InputHandler", "You dragged to Pos X: "+screenX+", Y: "+screenY);
+        return super.touchDragged(screenX, screenY, pointer);
+    }
+
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+        Gdx.app.log("InputHandler", "touchDown");
+        return false;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        Gdx.app.log("InputHandler", "tapped");
+        return false;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        return false;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+        return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
+
+    @Override
+    public void pinchStop() {
+
     }
 }
