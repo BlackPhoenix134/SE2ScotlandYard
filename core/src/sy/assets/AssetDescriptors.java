99115639a4433560e5063d77f32fb27517a9e847
BlackPhoenix134 <fabian.hermann@hotmail.com>
2021-04-26 16:57:03 +0200

    bug fix

--- core/src/sy/assets/AssetDescriptors.java
+++ core/src/sy/assets/AssetDescriptors.java
@@ -7,7 +7,7 @@ public class AssetDescriptors {
     //startscreen
     public static final AssetDescriptor<Texture> BUTTON_DEVIL = new AssetDescriptor<>(Assets.DEVILBUTTON, Texture.class);
     public static final AssetDescriptor<Texture> BUTTON_OPTIONS = new AssetDescriptor<>(Assets.OptionsDevilButton, Texture.class);
-    public static final AssetDescriptor<Texture> BUTTON_EXIT = new AssetDescriptor<>(Assets.DEVILBUTTON, Texture.class);
+    public static final AssetDescriptor<Texture> BUTTON_EXIT = new AssetDescriptor<>(Assets.ExitDevilButton, Texture.class);
 
 
     //gameboard
