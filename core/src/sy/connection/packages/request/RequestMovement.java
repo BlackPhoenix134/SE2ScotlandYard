75ee5fad5618931c47a5bc727eddb5d6a3ba8841
Johannes Ladurner <80148992+JohannesLadurner@users.noreply.github.com>
2021-05-15 21:24:36 +0200

    added RequestMovement class

---
+++ core/src/sy/connection/packages/request/RequestMovement.java
@@ -0,0 +1,39 @@
+package sy.connection.packages.request;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class RequestMovement {
+    private Vector2 originPosition;
+    private Vector2 targetPosition;
+    private int playerID;
+
+    public RequestMovement(int playerID, Vector2 originPosition, Vector2 targetPosition) {
+        this.playerID = playerID;
+        this.originPosition = originPosition;
+        this.targetPosition = targetPosition;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public Vector2 getOriginPosition() {
+        return originPosition;
+    }
+
+    public void setOriginPosition(Vector2 originPosition) {
+        this.originPosition = originPosition;
+    }
+
+    public Vector2 getTargetPosition() {
+        return targetPosition;
+    }
+
+    public void setTargetPosition(Vector2 targetPosition) {
+        this.targetPosition = targetPosition;
+    }
+}
