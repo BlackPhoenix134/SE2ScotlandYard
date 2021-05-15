729a4554ddc443e26c7ae3808dc7ee9ee8196342
Johannes Ladurner <80148992+JohannesLadurner@users.noreply.github.com>
2021-05-15 22:22:03 +0200

    implemented and set up Listener in Client, added send method

--- core/src/sy/connection/ClientHandler.java
+++ core/src/sy/connection/ClientHandler.java
@@ -9,25 +9,28 @@ import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 
+import sy.connection.packages.request.PlayerMovement;
 import sy.connection.packages.request.SomeRequest;
 import sy.connection.packages.response.SomeResponse;
 import sy.screens.GameScreen;
 import sy.screens.ScreenManager;
 
-public class ClientHandler {
+public class ClientHandler extends Listener {
 
+    static Client client;
 
     public void clientStart(){
-        Client client = new Client();
+        client = new Client();
         client.start();
 
-        //Kryo kryo = client.getKryo();
+        Kryo kryo = client.getKryo();
         //kryo.register(SomeRequest.class);
         //kryo.register(SomeResponse.class);
+        kryo.register(PlayerMovement.class);
 
         try {
             client.connect(5000, "192.168.0.136", 54555, 54777);
-
+            client.addListener(new ClientHandler());
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -47,4 +50,13 @@ public class ClientHandler {
         });*/
 
     }
+
+    public static void send(Object object){
+        if(object instanceof PlayerMovement){
+            PlayerMovement playerMovement = (PlayerMovement) object;
+            client.sendTCP(playerMovement);
+        }
+    }
+
+
 }
