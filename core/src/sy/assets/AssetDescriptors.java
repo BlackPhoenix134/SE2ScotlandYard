fb5294578f3887ee2adbbc90eb71c64207ffcb6d
tina <kristina.liebhart@gmail.com>
2021-05-26 11:24:27 +0200

    added tickets

--- core/src/sy/assets/AssetDescriptors.java
+++ core/src/sy/assets/AssetDescriptors.java
@@ -9,6 +9,7 @@ public final class AssetDescriptors {
     private static final String UI = "ui";
     private static final String WORLD = "world";
     private static final String CHARACTERS = WORLD + "/characters";
+    private static final String TICKETS = WORLD + "/tickets";
 
     public static final AssetDescriptor<Texture> BUTTON_DEVIL = new AssetDescriptor<>(UI + "/StartDevil.png", Texture.class);
     public static final AssetDescriptor<Texture> BUTTON_EXIT = new AssetDescriptor<>(UI + "/ExitDevilButton.png", Texture.class);
@@ -26,4 +27,10 @@ public final class AssetDescriptors {
     public static final AssetDescriptor<Texture> MONSTER3 = new AssetDescriptor<>(CHARACTERS + "/monster3.png", Texture.class);
     public static final AssetDescriptor<Texture> GHOST_WALKING = new AssetDescriptor<>( CHARACTERS + "/ghostWalking.png", Texture.class);
     public static final AssetDescriptor<Texture> SPIDER_WALKING = new AssetDescriptor<>(CHARACTERS + "/spiderWalking.png", Texture.class);
+
+    public static final AssetDescriptor<Texture> BIKE = new AssetDescriptor<> (TICKETS + "/bike.png", Texture.class);
+    public static final AssetDescriptor<Texture> BLACK_TICKET = new AssetDescriptor<> (TICKETS + "/blackticket.png", Texture.class);
+    public static final AssetDescriptor<Texture> DOUBLE_TICKET = new AssetDescriptor<> (TICKETS + "/doubleticket.png", Texture.class);
+    public static final AssetDescriptor<Texture> DRAGON = new AssetDescriptor<> (TICKETS + "/dragon.png", Texture.class);
+    public static final AssetDescriptor<Texture> HORSE = new AssetDescriptor<> (TICKETS + "/horse.png", Texture.class);
 }
