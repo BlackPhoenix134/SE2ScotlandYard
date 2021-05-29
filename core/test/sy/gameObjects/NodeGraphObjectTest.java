4fcdd6db4d13b1805c0db4dc169ab109c2b0b6dc
tina <kristina.liebhart@gmail.com>
2021-05-29 22:17:34 +0200

    added jacoco test report for sonarcube

---
+++ core/test/sy/gameObjects/NodeGraphObjectTest.java
@@ -0,0 +1,15 @@
+package sy.gameObjects;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeGraphObjectTest {
+
+    @Test
+    public void initialTest(){
+        assertTrue(true);
+    }
+
+}
