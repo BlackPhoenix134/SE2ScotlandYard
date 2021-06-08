69473c344ed9750f3dcbec35e43dbcf8269c7361
Samuel <miklau.samuel@gmail.com>
2021-06-08 13:22:41 +0200

    Added everything for EndScreens

--- core/src/sy/GameStart.java
+++ core/src/sy/GameStart.java
@@ -7,7 +7,7 @@ import sy.assets.SYAssetManager;
 import sy.assets.ShaderManager;
 import sy.platforms.CameraPeripheral;
 import sy.rendering.RenderPipeline;
-import sy.screens.GameEnd;
+import sy.screens.GameEndMrX;
 import sy.screens.GameScreen;
 import sy.screens.JoinGameMenu;
 import sy.screens.LobbyMenu;
@@ -38,7 +38,7 @@ public class GameStart extends com.badlogic.gdx.Game {
         screenManager.addScreen(new GameScreen(renderPipeline, camera, screenManager, shaderManager));
         screenManager.addScreen(new LobbyMenu(renderPipeline, camera, screenManager, cameraPeripheral));
         screenManager.addScreen(new JoinGameMenu(renderPipeline, camera, screenManager));
-        screenManager.addScreen(new GameEnd(renderPipeline, camera, screenManager));
+        screenManager.addScreen(new GameEndMrX(renderPipeline, camera, screenManager));
         screenManager.showScreen(MainMenuScreen.class);
     }
 
--- core/src/sy/screens/GameEndMrX.java
+++ core/src/sy/screens/GameEndMrX.java
@@ -3,16 +3,22 @@ package sy.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import sy.rendering.RenderPipeline;
 
-public class GameEnd extends AbstractScreen {
+public class GameEndMrX extends AbstractScreen {
     private float screenWidth;
     private float screenHeight;
     private ScreenManager screenManager;
     Sound sound = Gdx.audio.newSound(Gdx.files.internal("sounds/buttonSound.mp3"));
 
-    public GameEnd(RenderPipeline renderPipeline, OrthographicCamera camera, ScreenManager screenManager) {
+    SpriteBatch batch;
+    Texture img1, img2;
+    
+
+    public GameEndMrX(RenderPipeline renderPipeline, OrthographicCamera camera, ScreenManager screenManager) {
         this.screenManager = screenManager;
         screenWidth = Gdx.graphics.getWidth();
         screenHeight = Gdx.graphics.getHeight();
