9a93577564f1b2c0d0eb778837f3c1113b0e7332
BlackPhoenix134 <fabian.hermann@hotmail.com>
2021-06-13 15:55:48 +0200

    added better input system to handle priorities and consumables

---
+++ core/src/sy/input/prio/InputEventListener.java
@@ -0,0 +1,22 @@
+package sy.input.prio;
+
+
+import sy.core.Consumer;
+
+public class InputEventListener {
+    private Consumer<InputEvent> callback;
+    private int priority;
+
+    public InputEventListener(Consumer<InputEvent> callback, int priority) {
+        this.callback = callback;
+        this.priority = priority;
+    }
+
+    public void invoke(InputEvent event) {
+        callback.call(event);
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
---
+++ core/src/sy/input/prio/InputEventType.java
@@ -0,0 +1,5 @@
+package sy.input.prio;
+
+public enum InputEventType {
+    TOUCH_UP, TOUCH_DOWN, ZOOM, PAN
+}
