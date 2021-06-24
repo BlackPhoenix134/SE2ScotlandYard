ae58060ec471130dee9597258cc1621a60737ecf
Johannes Ladurner <80148992+JohannesLadurner@users.noreply.github.com>
2021-06-24 18:24:18 +0200

    added more detective tests

--- core/test/sy/core/GameplayDetectiveTest.java
+++ core/test/sy/core/GameplayDetectiveTest.java
@@ -32,6 +32,7 @@ class GameplayDetectiveTest {
         pawn = new PawnDetectiveObject();
         tickets = new DetectiveTickets(11, 8, 4);
         pawn.setTickets(tickets);
+        pawn.setMapNode(nodeGraphObject.getMapNodes().get(0));
 
         PawnDetectiveObject detective = new PawnDetectiveObject(); //collegue
         detective.setMapNode(nodeGraphObject.getMapNodes().get(1));
@@ -46,11 +47,75 @@ class GameplayDetectiveTest {
 
     @AfterEach
     void tearDown() {
+        gameplay = null;
+        pawn = null;
+        tickets = null;
+        detectives = null;
+        nodeGraphObject = null;
     }
 
     @Test
-    void detectiveCanMoveSuccess() {
-        pawn.setMapNode(nodeGraphObject.getMapNodes().get(0));
+    void canMoveSuccess() {
         assertTrue(gameplay.canMove(nodeGraphObject.getMapNodes().get(8), TicketType.BIKE));
     }
+
+    @Test
+    void canMoveNoEdge() {
+        assertFalse(gameplay.canMove(nodeGraphObject.getMapNodes().get(20), TicketType.BIKE));
+    }
+
+    @Test
+    void canMoveBikeSuccess() {
+        pawn.setMapNode(nodeGraphObject.getMapNodes().get(20));
+        assertTrue(gameplay.canMove(nodeGraphObject.getMapNodes().get(9), TicketType.BIKE));
+    }
+    @Test
+    void canMoveHorseSuccess() {
+        pawn.setMapNode(nodeGraphObject.getMapNodes().get(33));
+        assertTrue(gameplay.canMove(nodeGraphObject.getMapNodes().get(21), TicketType.HORSE));
+    }
+    @Test
+    void canMoveDragonSuccess() {
+        pawn.setMapNode(nodeGraphObject.getMapNodes().get(45));
+        assertTrue(gameplay.canMove(nodeGraphObject.getMapNodes().get(12), TicketType.DRAGON));
+    }
+    @Test
+    void canMoveBikeFailure() {
+        pawn.setMapNode(nodeGraphObject.getMapNodes().get(7));
+        assertFalse(gameplay.canMove(nodeGraphObject.getMapNodes().get(31), TicketType.BIKE));
+    }
+    @Test
+    void canMoveHorseFailure() {
+        pawn.setMapNode(nodeGraphObject.getMapNodes().get(0));
+        assertFalse(gameplay.canMove(nodeGraphObject.getMapNodes().get(8), TicketType.HORSE));
+    }
+    @Test
+    void canMoveDragonFailure() {
+        pawn.setMapNode(nodeGraphObject.getMapNodes().get(92));
+        assertFalse(gameplay.canMove(nodeGraphObject.getMapNodes().get(77), TicketType.DRAGON));
+    }
+
+    @Test
+    void canMoveBikeNoTickets() {
+        pawn.setMapNode(nodeGraphObject.getMapNodes().get(20));
+        pawn.setTickets(new DetectiveTickets(0, 1, 1));
+        assertFalse(gameplay.canMove(nodeGraphObject.getMapNodes().get(9), TicketType.BIKE));
+    }
+    @Test
+    void canMoveHorseNoTickets() {
+        pawn.setMapNode(nodeGraphObject.getMapNodes().get(33));
+        pawn.setTickets(new DetectiveTickets(1, 0, 1));
+        assertFalse(gameplay.canMove(nodeGraphObject.getMapNodes().get(21), TicketType.HORSE));
+    }
+    @Test
+    void canMoveDragonNoTickets() {
+        pawn.setMapNode(nodeGraphObject.getMapNodes().get(45));
+        pawn.setTickets(new DetectiveTickets(1, 1, 0));
+        assertFalse(gameplay.canMove(nodeGraphObject.getMapNodes().get(12), TicketType.DRAGON));
+    }
+    @Test
+    void canMoveDetectiveInTheWay(){
+        assertFalse(gameplay.canMove(nodeGraphObject.getMapNodes().get(1), TicketType.BIKE));
+    }
+
 }
