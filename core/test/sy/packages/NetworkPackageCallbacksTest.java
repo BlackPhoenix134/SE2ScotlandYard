8772e2f092395d2006c21203681cbd8eb760f965
BlackPhoenix134 <fabian.hermann@hotmail.com>
2021-06-25 10:39:07 +0200

    network package callbacks tests

--- core/test/sy/packages/NetworkPackageCallbacksTest.java
+++ core/test/sy/packages/NetworkPackageCallbacksTest.java
@@ -1,23 +1,21 @@
-package sy.connection;
+package sy.packages;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import sy.core.Consumer;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import sy.connection.NetworkPackageCallbacks;
 import static org.junit.jupiter.api.Assertions.*;
 
-class NetworkPackageCallbacksTest implements Consumer {
-    //values
-    //no values
-    //instance
+class NetworkPackageCallbacksTest {
     private NetworkPackageCallbacks instance;
 
     @BeforeEach
     void setUp() {
+        instance = new NetworkPackageCallbacks();
+        instance.registerCallback(Package1.class, pckg -> {
+            assertEquals(pckg.getClass(), Package1.class);
+        });
     }
 
     @AfterEach
@@ -25,24 +23,25 @@ class NetworkPackageCallbacksTest implements Consumer {
     }
 
     @Test
-    void registerCallbackTest(){
-        instance = new NetworkPackageCallbacks();
-        assertDoesNotThrow(()->{
-            instance.registerCallback(this.getClass(), this);
-        });
+    void registerTest(){
+        assertDoesNotThrow(() -> instance.registerCallback(Package2.class, pckg -> { }));
     }
 
     @Test
     void invokeTest(){
-        instance = new NetworkPackageCallbacks();
-        assertDoesNotThrow(()->{
-            instance.registerCallback(this.getClass(), this);
-            instance.invoke(this);
-        });
+        instance.invoke(new Package1());
     }
 
-    @Override
-    public void call(Object o) {
+    @Test
+    void invokeUnregisteredTest(){
+        assertDoesNotThrow(() ->  instance.invoke(new Package2()));
+    }
+
+    private class Package1 {
+
+    }
+
+    private class Package2 {
 
     }
 }
